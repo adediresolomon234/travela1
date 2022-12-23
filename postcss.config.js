883ab469e48af71d2cc99e4709fbcbdc666134b8
Adedire Solomon <63805569+adediresolomon234@users.noreply.github.com>
2022-12-23 15:03:52 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  },
+     },
+     loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.jsx?$/, loader: 'babel?stage=0', exclude: /node_modules/ },
+    ]
 }
