81633832209729c6c79545013ee4eb1e611055f5
Adedire Solomon <63805569+adediresolomon234@users.noreply.github.com>
2022-12-23 15:03:29 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,9 +20,9 @@ export default function Home({ exploreData,cardsData} ) {
       </Head>
 
       <Header />
-      <section id ="hero">
-      <Banner />
-      </section>
+        <section id ="hero">
+        <Banner />
+        </section>
 
       
       <Mainbanner />
