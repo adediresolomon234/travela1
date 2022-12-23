e7fc0f4463b949cc2f5b060b40b91966fffdcd5c
Adedire Solomon <63805569+adediresolomon234@users.noreply.github.com>
2022-12-23 15:04:08 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extend: {
       fontFamily:{
        Raleway:[
-          "Raleway,Arial,Helvetica,san-sans-serif",
+          "Montserrat Alternates, sans-serif",
           {fontFeatureSettings: '"cv11","ss01'}
         ]
       }
